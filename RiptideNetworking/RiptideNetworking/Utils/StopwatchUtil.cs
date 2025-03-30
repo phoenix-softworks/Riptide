cc42922c3160a352c23a9be046371a9be13bacb9
KptKosmit91 <kptkosmit91@o2.pl>
2025-03-30 18:58:04 +0200

    Update StopwatchUtil.cs

--- RiptideNetworking/RiptideNetworking/Utils/StopwatchUtil.cs
+++ RiptideNetworking/RiptideNetworking/Utils/StopwatchUtil.cs
@@ -1,8 +1,10 @@
-﻿namespace System.Diagnostics
+﻿using System.Diagnostics;
+
+namespace Riptide.Utils
 {
     internal static class StopwatchUtil
     {
-        // KK91: Framework 3.5 lacks the `Reset` function in stopwatch, so I reimplemented it here
+        // KK91: Framework 3.5 lacks the `Restart` function in stopwatch, so I reimplemented it here
         internal static void Restart(this Stopwatch watch)
         {
             watch.Stop();
