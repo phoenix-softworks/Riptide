8dee3d1c3b9eefb5b8c0e8255998d1a7927b5a22
KptKosmit91 <kptkosmit91@o2.pl>
2025-03-30 18:51:05 +0200

    Stopwatch Reset implementation (as extension method)

---
+++ RiptideNetworking/RiptideNetworking/Utils/StopwatchUtil.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Riptide.Utils
+{
+    internal static class StopwatchUtil
+    {
+        // KK91: Framework 3.5 lacks the `Reset` function in stopwatch, so I reimplemented it here
+        internal static void Reset(this Stopwatch watch)
+        {
+            watch.Stop();
+            watch.Reset();
+            watch.Start();
+        }
+    }
+}
