c300703d95da7ad0531385ea315639691a9aecc7
KptKosmit91 <kptkosmit91@o2.pl>
2025-03-30 18:52:42 +0200

    fix mistake Reset --> Restart

--- RiptideNetworking/RiptideNetworking/Utils/StopwatchUtil.cs
+++ RiptideNetworking/RiptideNetworking/Utils/StopwatchUtil.cs
@@ -3,7 +3,7 @@
     internal static class StopwatchUtil
     {
         // KK91: Framework 3.5 lacks the `Reset` function in stopwatch, so I reimplemented it here
-        internal static void Reset(this Stopwatch watch)
+        internal static void Restart(this Stopwatch watch)
         {
             watch.Stop();
             watch.Reset();
